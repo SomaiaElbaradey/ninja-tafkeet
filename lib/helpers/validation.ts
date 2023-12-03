82b118bc65c8f61242a01a87fb7ef01b159c7787
somaiaelbaradey <somayaelbaradey@gmail.com>
2023-12-03 03:41:13 +0200

    chore: re-organize the lib

--- lib/helpers/validation.ts
+++ lib/helpers/validation.ts
@@ -3,11 +3,11 @@ import { MAX_DIGITS_LENGTH } from "../constants";
 /**
  * Never operate on undefined (value - types)
  */
-const typeofDigitsIsDefined = (number: string | number): boolean =>
+export const typeofDigitsIsDefined = (number: string | number): boolean =>
     typeof number !== "undefined";
 
 /**
  * Digits are numbers and not more than millions
  */
-const validDigits = (digits: string): boolean =>
+export const validDigits = (digits: string): boolean =>
     /^[0-9]+$/.test(digits) && digits.length <= MAX_DIGITS_LENGTH;
