fe18e539d828bb91dddfa5bf92e73ef9e8291145
somaiaelbaradey <somayaelbaradey@gmail.com>
2023-12-03 02:16:30 +0200

    first commit

---
+++ index.ts
@@ -0,0 +1,106 @@
+/**
+ * tafkeet-ninja module.
+ * @module tafkeet-ninja
+ * @description Converts currency digits into written Arabic words
+ * @author Somaia Elbaradey
+ */
+
+// import * as bla from './constants'
+// export const tafkeet = (number: number | string) => {
+
+    
+//     // see the length of the number 
+// }
+
+// export = React;
+// export as namespace React;
+
+// declare namespace React {
+//     //
+//     // React Elements
+//     // ----------------------------------------------------------------------
+
+//     type ElementType<P = any> =
+//         {
+//             [K in keyof JSX.IntrinsicElements]: P extends JSX.IntrinsicElements[K] ? K : never
+//         }[keyof JSX.IntrinsicElements] |
+//         ComponentType<P>;
+//     type ComponentType<P = {}> = ComponentClass<P> | FunctionComponent<P>;
+
+// export class Redirect extends React.Component<RedirectProps, any> {}
+
+// export function generatePath<S extends string>(path: S, params?: ExtractRouteParams<S>): string;
+
+
+interface MathFuncs {
+    divide(a: number, b: number): number;
+    multiply(a: number, b: number): number;
+  }
+  
+  const mathFunctions: MathFuncs = {
+    divide: (a, b) => a / b,
+    multiply: (a, b) => a * b
+  };
+  
+  export = mathFunctions;
+
+
+//   var json_stringify = require('./lib/stringify.js').stringify;
+// var json_parse     = require('./lib/parse.js');
+
+// module.exports = function(options) {
+//     return  {
+//         parse: json_parse(options),
+//         stringify: json_stringify
+//     }
+// };
+// //create the default method members with no options applied for backwards compatibility
+// module.exports.parse = json_parse();
+// module.exports.stringify = json_stringify;
+// Default options one can override by passing options to the parse()
+
+// declared but never assigned a value or declared and assigned the value of undefined
+// if(typeof mathFunctions === 'undefined')
+
+// export declare const VERSION: any;
+// export declare type NodeModuleEmitKind = 'nodeesm' | 'nodecjs';
+// import type * as _ts from 'typescript';
+// transformers?: _ts.CustomTransformers | ((p: _ts.Program) => _ts.CustomTransformers);
+
+
+// declare const axios: AxiosStatic;
+// types: index.d.ts
+// index.js: exports
+// const {
+//   Axios,
+// } = axios;
+
+// export {
+//   axios as default,
+//   Axios,
+// }
+
+// dist/ minified version of 
+
+// This module is intended to unwrap Axios default export as named.
+// Keep top-level export same with static properties
+// so that it can keep same with es module or cjs
+
+// "exports": {
+//     ".": {
+//       "types": {
+//         "require": "./index.d.cts",
+//         "default": "./index.d.ts"
+//       },
+//       "browser": {
+//         "require": "./dist/browser/axios.cjs",
+//         "default": "./index.js"
+//       },
+//       "default": {
+//         "require": "./dist/node/axios.cjs",
+//         "default": "./index.js"
+//       }
+//     },
+
+// Webpack
+// Rollup
