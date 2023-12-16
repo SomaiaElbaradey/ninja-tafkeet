a47a28127ef6f12fc42c20af74a9adf6b4f0a5de
somaiaelbaradey <somayaelbaradey@gmail.com>
2023-12-16 10:36:57 +0200

    chore: linting & create test file

--- lib/helpers/types.ts
+++ lib/helpers/types.ts
@@ -2,9 +2,9 @@ import { ONES } from "../constants";
 
 export type Ones = keyof typeof ONES;
 export type Currency = {
-  singular: string;
-  plural: string;
-  fraction: string;
-  fractions: string;
-  decimals: number;
+    singular: string;
+    plural: string;
+    fraction: string;
+    fractions: string;
+    decimals: number;
 };
