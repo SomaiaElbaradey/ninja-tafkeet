42012531a02689ad522e17e81f0439a37cc71810
somaiaelbaradey <somayaelbaradey@gmail.com>
2023-12-26 17:27:14 +0200

    tests: test cases

--- lib/tests/index.test.ts
+++ lib/tests/index.test.ts
@@ -1,134 +1,129 @@
-import { assert, describe, expect, it } from 'vitest'
-import { tafkeet } from '../tafkeet'
+import { describe, expect, it } from 'vitest'
+import { baseTafkeet } from '../tafkeet'
 
-describe('Reading numbers', () => {
+describe('Reading (ones, tens, hundreds, thousands) numbers', () => {
   it('should read 1', () => {
-    expect(tafkeet(1)).toBe('واحد')
+    expect(baseTafkeet(1)).toBe('واحد')
   })
   it('should read 0', () => {
-    expect(tafkeet(0)).toBe('صفر')
+    expect(baseTafkeet(0)).toBe('صفر')
   })
   it('should read 11', () => {
-    expect(tafkeet(11)).toBe('أحدَ عشر')
+    expect(baseTafkeet(11)).toBe('أحدَ عشر')
   })
   it('should read 30', () => {
-    expect(tafkeet(30)).toBe('ثلاثون')
+    expect(baseTafkeet(30)).toBe('ثلاثون')
   })
   it('should read 96', () => {
-    expect(tafkeet(96)).toBe('ستة وتسعون')
+    expect(baseTafkeet(96)).toBe('ستة وتسعون')
   })
   it('should read 101', () => {
-    expect(tafkeet(101)).toBe('مائة وواحد')
+    expect(baseTafkeet(101)).toBe('مائة وواحد')
   })
   it('should read 999', () => {
-    expect(tafkeet(999)).toBe('تسعمائة وتسعة وتسعون')
+    expect(baseTafkeet(999)).toBe('تسعمائة وتسعة وتسعون')
   })
   it('should read 600', () => {
-    expect(tafkeet(600)).toBe('ستمائة')
+    expect(baseTafkeet(600)).toBe('ستمائة')
   })
   it('should read 1,000', () => {
-    expect(tafkeet(1000)).toBe('ألف')
+    expect(baseTafkeet(1000)).toBe('ألف')
   })
   it('should read 2,000', () => {
-    expect(tafkeet(2000)).toBe('ألفان')
+    expect(baseTafkeet(2000)).toBe('ألفان')
   })
   it('should read 3,456', () => {
-    expect(tafkeet(3456)).toBe('ثلاثة آلاف وأربعمائة وستة وخمسون')
+    expect(baseTafkeet(3456)).toBe('ثلاثة آلاف وأربعمائة وستة وخمسون')
   })
   it('should read 9,056', () => {
-    expect(tafkeet(9056)).toBe('تسعة آلاف وستة وخمسون')
+    expect(baseTafkeet(9056)).toBe('تسعة آلاف وستة وخمسون')
   })
   it('should read 9,006', () => {
-    expect(tafkeet(9006)).toBe('تسعة آلاف وستة')
+    expect(baseTafkeet(9006)).toBe('تسعة آلاف وستة')
   })
   it('should read 1,001', () => {
-    expect(tafkeet(1001)).toBe('ألف وواحد')
+    expect(baseTafkeet(1001)).toBe('ألف وواحد')
   })
   it('should read 1,011', () => {
-    expect(tafkeet(1011)).toBe('ألف وأحدَ عشر')
+    expect(baseTafkeet(1011)).toBe('ألف وأحدَ عشر')
   })
   it('should read 10,001', () => {
-    expect(tafkeet(10001)).toBe('عشر آلاف وواحد')
+    expect(baseTafkeet(10001)).toBe('عشر آلاف وواحد')
   })
   it('should read 10,010', () => {
-    expect(tafkeet(10010)).toBe('عشر آلاف وعشر')
+    expect(baseTafkeet(10010)).toBe('عشر آلاف وعشر')
   })
   it('should read 10,011', () => {
-    expect(tafkeet(10011)).toBe('عشر آلاف وأحدَ عشر')
+    expect(baseTafkeet(10011)).toBe('عشر آلاف وأحدَ عشر')
   })
   it('should read 10,000', () => {
-    expect(tafkeet(10000)).toBe('عشر آلاف')
+    expect(baseTafkeet(10000)).toBe('عشر آلاف')
   })
   it('should read 11,000', () => {
-    expect(tafkeet(11000)).toBe('أحدَ عشر ألفًا')
+    expect(baseTafkeet(11000)).toBe('أحدَ عشر ألفًا')
   })
   it('should read 30,000', () => {
-    expect(tafkeet(30000)).toBe('ثلاثون ألفًا')
+    expect(baseTafkeet(30000)).toBe('ثلاثون ألفًا')
   })
   it('should read 30,001', () => {
-    expect(tafkeet(30001)).toBe('ثلاثون ألفًا وواحد')
+    expect(baseTafkeet(30001)).toBe('ثلاثون ألفًا وواحد')
   })
   it('should read 30,955', () => {
-    expect(tafkeet(30955)).toBe('ثلاثون ألفًا وتسعمائة وخمسة وخمسون')
+    expect(baseTafkeet(30955)).toBe('ثلاثون ألفًا وتسعمائة وخمسة وخمسون')
   })
   it('should read 65,900', () => {
-    expect(tafkeet(65900)).toBe('خمسة وستون ألفًا وتسعمائة')
+    expect(baseTafkeet(65900)).toBe('خمسة وستون ألفًا وتسعمائة')
   })
   it('should read 101,026', () => {
-    expect(tafkeet(101026)).toBe('مائة وواحد ألفًا وستة وعشرون')
+    expect(baseTafkeet(101026)).toBe('مائة وواحد ألفًا وستة وعشرون')
   })
   it('should read 101,000', () => {
-    expect(tafkeet(101000)).toBe('مائة وواحد ألفًا')
+    expect(baseTafkeet(101000)).toBe('مائة وواحد ألفًا')
   })
   it('should read 101,100', () => {
-    expect(tafkeet(101100)).toBe('مائة وواحد ألفًا ومائة')
+    expect(baseTafkeet(101100)).toBe('مائة وواحد ألفًا ومائة')
   })
   it('should read 100,010', () => {
-    expect(tafkeet(100010)).toBe('مائة ألفًا وعشر')
+    expect(baseTafkeet(100010)).toBe('مائة ألفًا وعشر')
   })
-  it('should read 2,000,000', () => {
-    expect(tafkeet(2000000)).toBe('مليونان')
+  it('should read 120,066', () => {
+    expect(baseTafkeet(120066)).toBe('مائة وعشرون ألفًا وستة وستون')
   })
-  it('should read 1,000,000', () => {
-    expect(tafkeet(1000000)).toBe('مليون')
+  it('should read 120,006', () => {
+    expect(baseTafkeet(120006)).toBe('مائة وعشرون ألفًا وستة')
   })
-  it('should read 1,100,000', () => {
-    expect(tafkeet(1100000)).toBe('مليون ومائة ألفًا')
+  it('should read 101,496', () => {
+    expect(baseTafkeet(101496)).toBe('مائة وواحد ألفًا وأربعمائة وستة وتسعون')
   })
-  it('should read 1,101,100', () => {
-    expect(tafkeet(1101100)).toBe('مليون ومائة وواحد ألفًا ومائة')
+  it('should read 120,000', () => {
+    expect(baseTafkeet(120000)).toBe('مائة وعشرون ألفًا')
   })
-  it('should read 1,001,100', () => {
-    expect(tafkeet(1001100)).toBe('مليون وألف ومائة')
+  it('should read 100,000', () => {
+    expect(baseTafkeet(100000)).toBe('مائة ألفًا')
   })
-  it('should read 6,010,000', () => {
-    expect(tafkeet(6010000)).toBe('ستة ملايين وعشر آلاف')
+  it('should read 21,007', () => {
+    expect(baseTafkeet(21007)).toBe('واحد وعشرون ألفًا وسبعة')
   })
-  it('should read 66,090,900', () => {
-    expect(tafkeet(66090900)).toBe('ستة وستون مليونًا وتسعون ألفًا وتسعمائة')
+  it('should read 20,263', () => {
+    expect(baseTafkeet(20263)).toBe('عشرون ألفًا ومئتان وثلاثة وستون')
   })
-  // it('should read 6010000', () => {
-  //   expect(tafkeet(6010000)).toBe('ستة ملايين وعشر آلاف')
-  // })
-  // it('should read 6010000', () => {
-  //   expect(tafkeet(6010000)).toBe('ستة ملايين وعشر آلاف')
-  // })
-  // it('should read 6010000', () => {
-  //   expect(tafkeet(6010000)).toBe('ستة ملايين وعشر آلاف')
-  // })
-})
 
-describe('Reading digits', function () {
-  it('should read 1', function () {
-    assert.equal('واحد', tafkeet(1))
+  it('should read 19,000', () => {
+    expect(baseTafkeet(19000)).toBe('تسعة عشر ألفًا')
   })
-  it('should read 0', function () {
-    assert.equal('صفر', tafkeet(0))
+  it('should read 1,105', () => {
+    expect(baseTafkeet(1105)).toBe('ألف ومائة وخمسة')
   })
-  it('should read 9006', function () {
-    assert.equal('تسعة آلاف وستة', tafkeet(9006))
+  it('should read 122', () => {
+    expect(baseTafkeet(122)).toBe('مائة واثنان وعشرون')
   })
-  it('should read 3456', () => {
-    assert.equal('ثلاثة آلاف وأربعمائة وستة وخمسون', tafkeet(3456))
+  it('should read 100', () => {
+    expect(baseTafkeet(100)).toBe('مائة')
+  })
+  it('should read 96', () => {
+    expect(baseTafkeet(96)).toBe('ستة وتسعون')
+  })
+  it('should read 10', () => {
+    expect(baseTafkeet(10)).toBe('عشر')
   })
 })
--- lib/tests/millions.test.ts
+++ lib/tests/millions.test.ts
@@ -1,41 +1,126 @@
 import { assert, describe, expect, it } from 'vitest'
-import { tafkeet } from '../tafkeet'
+import { baseTafkeet } from '../tafkeet'
 
 describe('Reading millions', () => {
   it('should read 2,000,000', () => {
-    expect(tafkeet(2000000)).toBe('مليونان')
+    expect(baseTafkeet(2000000)).toBe('مليونان')
   })
   it('should read 1,000,000', () => {
-    expect(tafkeet(1000000)).toBe('مليون')
+    expect(baseTafkeet(1000000)).toBe('مليون')
   })
   it('should read 1,100,000', () => {
-    expect(tafkeet(1100000)).toBe('مليون ومائة ألفًا')
+    expect(baseTafkeet(1100000)).toBe('مليون ومائة ألفًا')
   })
   it('should read 1,101,100', () => {
-    expect(tafkeet(1101100)).toBe('مليون ومائة وواحد ألفًا ومائة')
+    expect(baseTafkeet(1101100)).toBe('مليون ومائة وواحد ألفًا ومائة')
   })
   it('should read 1,001,100', () => {
-    expect(tafkeet(1001100)).toBe('مليون وألف ومائة')
+    expect(baseTafkeet(1001100)).toBe('مليون وألف ومائة')
   })
   it('should read 6,010,000', () => {
-    expect(tafkeet(6010000)).toBe('ستة ملايين وعشر آلاف')
+    expect(baseTafkeet(6010000)).toBe('ستة ملايين وعشر آلاف')
   })
   it('should read 66,090,900', () => {
-    expect(tafkeet(66090900)).toBe('ستة وستون مليونًا وتسعون ألفًا وتسعمائة')
+    expect(baseTafkeet(66090900)).toBe(
+      'ستة وستون مليونًا وتسعون ألفًا وتسعمائة'
+    )
   })
-  // it('should read 6010000', () => {
-  //   expect(tafkeet(6010000)).toBe('ستة ملايين وعشر آلاف')
-  // })
-  // it('should read 6010000', () => {
-  //   expect(tafkeet(6010000)).toBe('ستة ملايين وعشر آلاف')
-  // })
-  // it('should read 6010000', () => {
-  //   expect(tafkeet(6010000)).toBe('ستة ملايين وعشر آلاف')
-  // })
+  it('should read 3,044,092', () => {
+    expect(baseTafkeet(3044092)).toBe(
+      'ثلاثة ملايين وأربعة وأربعون ألفًا واثنان وتسعون'
+    )
+  })
+  it('should read 3,014,636', () => {
+    expect(baseTafkeet(3014636)).toBe(
+      'ثلاثة ملايين وأربعة عشر ألفًا وستمائة وستة وثلاثون'
+    )
+  })
+  it('should read 3,151,504', () => {
+    expect(baseTafkeet(3151504)).toBe(
+      'ثلاثة ملايين ومائة وواحد وخمسون ألفًا وخمسمائة وأربعة'
+    )
+  })
+  it('should read 3,029,242', () => {
+    expect(baseTafkeet(3029242)).toBe(
+      'ثلاثة ملايين وتسعة وعشرون ألفًا ومئتان واثنان وأربعون'
+    )
+  })
+  it('should read 3,129,247', () => {
+    expect(baseTafkeet(3129247)).toBe(
+      'ثلاثة ملايين ومائة وتسعة وعشرون ألفًا ومئتان وسبعة وأربعون'
+    )
+  })
+  it('should read 3,120,500', () => {
+    expect(baseTafkeet(3120500)).toBe(
+      'ثلاثة ملايين ومائة وعشرون ألفًا وخمسمائة'
+    )
+  })
+  it('should read 3,100,000', () => {
+    expect(baseTafkeet(3100000)).toBe('ثلاثة ملايين ومائة ألفًا')
+  })
+  it('should read 3,000,000', () => {
+    expect(baseTafkeet(3000000)).toBe('ثلاثة ملايين')
+  })
+  it('should read 2,044,092', () => {
+    expect(baseTafkeet(2044092)).toBe(
+      'مليونان وأربعة وأربعون ألفًا واثنان وتسعون'
+    )
+  })
+  it('should read 2,014,636', () => {
+    expect(baseTafkeet(2014636)).toBe(
+      'مليونان وأربعة عشر ألفًا وستمائة وستة وثلاثون'
+    )
+  })
+  it('should read 2,151,504', () => {
+    expect(baseTafkeet(2151504)).toBe(
+      'مليونان ومائة وواحد وخمسون ألفًا وخمسمائة وأربعة'
+    )
+  })
+  it('should read 2,129,247', () => {
+    expect(baseTafkeet(2129247)).toBe(
+      'مليونان ومائة وتسعة وعشرون ألفًا ومئتان وسبعة وأربعون'
+    )
+  })
+  it('should read 2,029,242', () => {
+    expect(baseTafkeet(2029242)).toBe(
+      'مليونان وتسعة وعشرون ألفًا ومئتان واثنان وأربعون'
+    )
+  })
+  it('should read 2,120,500', () => {
+    expect(baseTafkeet(2120500)).toBe('مليونان ومائة وعشرون ألفًا وخمسمائة')
+  })
+  it('should read 2,100,000', () => {
+    expect(baseTafkeet(2100000)).toBe('مليونان ومائة ألفًا')
+  })
+  it('should read 1,000,005', () => {
+    expect(baseTafkeet(1000005)).toBe('مليون وخمسة')
+  })
+  it('should read 1,000,500', () => {
+    expect(baseTafkeet(1000500)).toBe('مليون وخمسمائة')
+  })
+  it('should read 1,014,636', () => {
+    expect(baseTafkeet(1014636)).toBe(
+      'مليون وأربعة عشر ألفًا وستمائة وستة وثلاثون'
+    )
+  })
+  it('should read 1,192,928', () => {
+    expect(baseTafkeet(1192928)).toBe(
+      'مليون ومائة واثنان وتسعون ألفًا وتسعمائة وثمانية وعشرون'
+    )
+  })
+  it('should read 1,120,500', () => {
+    expect(baseTafkeet(1120500)).toBe('مليون ومائة وعشرون ألفًا وخمسمائة')
+  })
 })
 
 describe('Reading millions digits', function () {
-  it('should read 1', function () {
-    assert.equal('واحد', tafkeet(1))
+  it('should read 2,000,000', function () {
+    assert.equal('مليونان', baseTafkeet(2000000))
+  })
+  it('should read 3,044,092', function () {
+    assert.equal(
+      'ثلاثة ملايين وأربعة وأربعون ألفًا واثنان وتسعون',
+      baseTafkeet(3044092)
+    )
   })
 })
