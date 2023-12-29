16c95f17af3483516077ba5fc9297f5166f79860
somaiaelbaradey <somayaelbaradey@gmail.com>
2023-12-29 22:30:59 +0200

    test: Reading fractions

--- lib/tests/fraction/index.test.ts
+++ lib/tests/fraction/index.test.ts
@@ -2,10 +2,19 @@ import { assert, describe, expect, it } from 'vitest'
 import { wholeStringTafkeet } from '../../tafkeet'
 
 describe('Reading fractions', () => {
-  //   it('should read 2,000,000', () => {
-  //     expect(wholeStringTafkeet(2000000).fractionTafkeet).toBe('مليونان')
-  //   })
   it('should read 1.1', () => {
     expect(wholeStringTafkeet(1.1).fractionTafkeet).toBe('واحد')
+  })
+  it('should read 122.33', () => {
+    expect(wholeStringTafkeet(122.33).fractionTafkeet).toBe('ثلاثة وثلاثون')
+  })
+  it('should read 122.33', () => {
+    expect(wholeStringTafkeet(122.33).fractionTafkeet).toBe('ثلاثة وثلاثون')
+  })
+  it('should read 122.30', () => {
+    expect(wholeStringTafkeet('122.30').fractionTafkeet).toBe('ثلاثون')
+  })
+  it('should read 1.2', () => {
+    expect(wholeStringTafkeet(1.2).fractionTafkeet).toBe('اثنان')
   })
 })
